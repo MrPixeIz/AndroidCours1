f3ebc7bae1db7a61f80f1f65b1870e5a298b402f
marc <marcscolaire@gmail.com>
2020-03-13 10:40:40 -0400

    commit finale

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     private GuessNumber guessNumber;
     private EditText inputNumber;
     private Button buttonTry;
+    private Button buttonCompare;
+    private EditText inputNumber1;
+    private EditText inputNumber2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         inputNumber=findViewById(R.id.editTextGuessNumber);
         buttonTry=findViewById(R.id.buttonTryNumber);
+        buttonCompare=findViewById(R.id.buttonCompare);
+        inputNumber1=findViewById(R.id.editTextNum1);
+        inputNumber2=findViewById(R.id.editTextNum2);
 
         guessNumber=new GuessNumber(MAX);
         setListenner();
@@ -33,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
                 guess();
             }
         });
+        buttonCompare.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                compare();
+            }
+        });
+
     }
     private void guess(){
         int numberToGuess = Integer.parseInt(inputNumber.getText().toString());
@@ -51,5 +64,32 @@ public class MainActivity extends AppCompatActivity {
     }
     private void showHint(String hint){
         Toast.makeText(this, hint, Toast.LENGTH_SHORT).show();
+    }
+    private void compare()
+    {
+        try {
+            inputNumber1.setBackgroundResource(R.color.white);
+            inputNumber2.setBackgroundResource(R.color.white);
+            int number1= Integer.parseInt(inputNumber1.getText().toString());
+            int number2= Integer.parseInt(inputNumber2.getText().toString());
+            String message;
+            if (number1>number2)
+            {
+                message="Le nombre 1 est plus grand";
+                inputNumber1.setBackgroundResource(R.color.green);
+            }
+            else if (number1<number2)
+            {
+                message="Le nombre 2 est plus grand";
+                inputNumber2.setBackgroundResource(R.color.green);
+            }
+            else {
+                message="Les nombres sont égaux";
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+        catch(Exception ex){
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
